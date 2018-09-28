57f822161465a017c5854c24d24d13c813b7c442
ibelkebeer <ibelkebir@stuy.edu>
2018-09-28 10:37:19 -0400

    progress!

--- mystr.c
+++ mystr.c
@@ -1,1 +1,42 @@
 #include "mystr.h"
+#include <stdio.h>
+int my_strlen(char* str){
+  int num = 0;
+  while(*str){
+    num++;
+    str++;
+  }
+  return num;
+}
+
+char* my_strcpy(char* dest, char* src){
+  char* origin = src;
+  while(*src){
+    *dest = *src;
+    dest++;
+    src++;
+  }
+  return origin;
+}
+
+char* my_strncat(char* dest, char*src, int n){
+  char* origin = src;
+  while(*dest){
+    dest++;
+  }
+  while(*src || n){
+    *dest = *src;
+    dest++;
+    src++;
+    n--;
+  }
+  return origin;
+}
+
+int main(){
+  char* src = "hello";
+  char dest[256];
+  printf("Source: %s\nDest: %s\n",my_strcpy(dest,src),dest);
+  printf("Source: %s\nDest: %s\n",my_strncat(dest,src, 1),dest);
+  return 0;
+}
